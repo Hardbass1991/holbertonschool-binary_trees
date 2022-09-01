0c8021f4e0783aa8f1714bd832f4c12be2c3eb41
Franco Cardenas <gustavo260123@gmail.com>
2022-09-01 12:26:47 -0700

    Adding the file that remove an tree complete and some header changes.

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,11 @@
 #ifndef FUNCS
 #define FUNCS
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -18,6 +24,7 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+/*Function Helper*/
 void binary_tree_print(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
